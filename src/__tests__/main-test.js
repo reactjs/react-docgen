15737b319197cfefb229e5fd010ae0fe66f7c88a
Felix Kling <felix.kling@gmx.net>
2015-07-23 17:08:21 -0700

    Extend main test with class definition

--- src/__tests__/main-test.js
+++ src/__tests__/main-test.js
@@ -8,68 +8,93 @@
  *
  */
 
-"use strict";
+/*global jest, describe, beforeEach, it, expect*/
 
 jest.autoMockOff();
 
-var source = [
-  'var React = require("React");',
-  'var PropTypes = React.PropTypes;',
-  '/**',
-  ' * Example component description',
-  ' */',
-  'var Component = React.createClass({',
-  '  propTypes: {',
-  '    /**',
-  '     * Example prop description',
-  '     */',
-  '    foo: PropTypes.bool',
-  '  },',
-  '  getDefaultProps: function() {',
-  '    return {',
-  '      foo: true',
-  '    };',
-  '  }',
-  '});',
-  'module.exports = Component;'
-].join('\n');
-
 describe('main', () => {
-  var utils;
   var docgen;
 
   beforeEach(() => {
-    utils = require('../../tests/utils');
     docgen = require('../main');
   });
 
-  it('parses with default resolver/handlers', () => {
-    var docs = docgen.parse(source);
-    expect(docs).toEqual({
-      description: 'Example component description',
-      props: {
-        foo: {
-          type: {
-            name: 'bool'
+  function test(source) {
+    it('parses with default resolver/handlers', () => {
+      var docs = docgen.parse(source);
+      expect(docs).toEqual({
+        description: 'Example component description',
+        props: {
+          foo: {
+            type: {
+              name: 'bool',
+            },
+            defaultValue: {
+              computed: false,
+              value: 'true',
+            },
+            description: 'Example prop description',
+            required: false,
           },
-          defaultValue: {
-            computed: false,
-            value: 'true'
-          },
-          description: 'Example prop description',
-          required: false
-        }
-      }
+        },
+      });
     });
+
+    it('parses with custom handlers', () => {
+      var docs = docgen.parse(source, null, [
+        docgen.handlers.componentDocblockHandler,
+      ]);
+      expect(docs).toEqual({
+        description: 'Example component description',
+        props: {},
+      });
+    });
+  }
+
+  describe('React.createClass', () => {
+    test(`
+      var React = require("React");
+      var PropTypes = React.PropTypes;
+      /**
+       * Example component description
+       */
+      var Component = React.createClass({
+        propTypes: {
+          /**
+           * Example prop description
+           */
+          foo: PropTypes.bool
+        },
+        getDefaultProps: function() {
+          return {
+            foo: true
+          };
+        }
+      });
+      module.exports = Component
+    `);
   });
 
-  it('parses with custom handlers', () => {
-    var docs = docgen.parse(source, null, [
-      docgen.handlers.componentDocblockHandler,
-    ]);
-    expect(docs).toEqual({
-      description: 'Example component description',
-      props: {}
-    });
+  describe('Class definition', () => {
+    test(`
+      var React = require("React");
+      var PropTypes = React.PropTypes;
+      /**
+       * Example component description
+       */
+      export default class Component extends React.Component {
+        static propTypes = {
+          /**
+           * Example prop description
+           */
+          foo: PropTypes.bool
+        };
+        // ...
+      }
+      Component.defaultProps = {
+        foo: true,
+      };
+    `);
   });
+
 });
