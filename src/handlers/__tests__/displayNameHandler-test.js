09d541c61f012c79d69d9628d9c56b0e44c08a4c
Nathan Smith <natsmith@digitalriver.com>
2016-03-04 14:48:53 -0600

    Immediately Exported ES6 Modules passing tests

--- src/handlers/__tests__/displayNameHandler-test.js
+++ src/handlers/__tests__/displayNameHandler-test.js
@@ -99,16 +99,74 @@ describe('defaultPropsHandler', () => {
     displayNameHandler(documentation, definition);
     expect(documentation.displayName).toBe('Foo');
   });
+});
 
-  it('infers the displayName from Function Expression', () => {
-    var definition = expression('(function Foo() {})');
+describe('defaultPropsHandler with ES6 Exports', () => {
+  var documentation;
+  var displayNameHandler;
+  var expression, statement;
+
+  beforeEach(() => {
+    ({expression, statement} = require('../../../tests/utils'));
+    documentation = new (require('../../Documentation'));
+    displayNameHandler = require('../displayNameHandler');
+  });
+  it('extracts the displayName', () => {
+    var definition = statement(`
+      export class Foo {
+        static displayName = "BarFoo";
+      }
+    `);
     displayNameHandler(documentation, definition);
     expect(documentation.displayName).toBe('Foo');
   });
 
-  it('infers the displayName with named export', () => {
+  it('resolves identifiers', () => {
     var definition = statement(`
-      export var Foo = function() {}
+      export class Foo {
+        static displayName = name;
+      }
+      var name = 'xyz';
+    `);
+    displayNameHandler(documentation, definition);
+    expect(documentation.displayName).toBe('xyz');
+  });
+
+  it('ignores non-literal names', () => {
+    var definition = statement(`
+      export class Foo {
+        static displayName = foo.bar;
+      }
+    `);
+    expect(() => displayNameHandler(documentation, definition)).not.toThrow();
+    expect(documentation.displayName).not.toBeDefined();
+  });
+
+  it('infers the displayName with es6 class', () => {
+    var definition = statement(`
+      export class Foo {}
+    `);
+    displayNameHandler(documentation, definition);
+    expect(documentation.displayName).toBe('Foo');
+  });
+
+  it('infers the displayName with extended es6 class', () => {
+    var definition = statement(`
+      export class Foo extends Component {
+        render(){
+          return null
+        }
+      }
+    `);
+    displayNameHandler(documentation, definition);
+    expect(documentation.displayName).toBe('Foo');
+  });
+
+  it('infers the displayName with stateless functional component', () => {
+    var definition = statement(`
+      export var Foo = () => {
+        return <div>JSX</div>
+      }
     `);
     displayNameHandler(documentation, definition);
     expect(documentation.displayName).toBe('Foo');
--- src/handlers/displayNameHandler.js
+++ src/handlers/displayNameHandler.js
@@ -16,28 +16,48 @@ import getMemberValuePath from '../utils/getMemberValuePath';
 import resolveName from '../utils/resolveName';
 import recast from 'recast';
 import resolveToValue from '../utils/resolveToValue';
+import resolveToModule from '../utils/resolveToModule';
+import isExportsOrModuleAssignment from '../utils/isExportsOrModuleAssignment';
+import resolveExportDeclaration from '../utils/resolveExportDeclaration'
 
 var {types: {namedTypes: types}} = recast;
+
+function getOrInferDisplayName(path) {
+    var displayNamePath = getMemberValuePath(path, 'displayName');
+
+    if (displayNamePath) {
+      displayNamePath = resolveToValue(displayNamePath);
+      if (!displayNamePath || !types.Literal.check(displayNamePath.node)) {
+        return;
+      }
+      return displayNamePath.node.value;
+    } else if (!displayNamePath && path.node.id) {
+      return path.node.id.name;
+    } else if (!displayNamePath && resolveName(path)) {
+      return resolveName(path);
+    }
+}
 
 export default function displayNameHandler(
   documentation: Documentation,
   path: NodePath
 ) {
-  var displayNamePath = getMemberValuePath(path, 'displayName');
+  var displayNamePath;
 
-  if (displayNamePath) {
-    displayNamePath = resolveToValue(displayNamePath);
-    if (!displayNamePath || !types.Literal.check(displayNamePath.node)) {
-      return;
-    }
-    displayNamePath = displayNamePath.node.value
-  } else if (!displayNamePath && path.node.id) {
-    displayNamePath = path.node.id.name;
-  } else if (!displayNamePath && resolveName(path)){
-    displayNamePath = resolveName(path)
+  if (!types.ExportNamedDeclaration.check(path.node) && !isExportsOrModuleAssignment(path)) {
+    displayNamePath = getOrInferDisplayName(path);
   } else {
-    return
-  }
 
+    var declarationPath;
+    var declaration = path.node.declaration;
+
+    if (declaration.type === types.ClassDeclaration.name) {
+      declarationPath = resolveExportDeclaration(path)[0];
+    } else if (declaration.type === types.VariableDeclaration.name) {
+      declarationPath = resolveExportDeclaration(path)[0].parentPath.parentPath.parentPath;
+    }
+
+    displayNamePath = getOrInferDisplayName(declarationPath);
+  }
   documentation.set('displayName', displayNamePath);
 }
