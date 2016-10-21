a4a03c507ccc7574f0e690d6a291e41ba586787c
Felix Kling <felix.kling@gmx.net>
2016-10-21 12:58:58 -0700

    Add example from #131 to fixtures

---
+++ src/__tests__/fixtures/component_5.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Button = ({ children, onClick, style = {} }) => (
+  <button
+    style={{ }}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  children: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func,
+  style: React.PropTypes.object,
+};
+
+export default Button;
