98f18e57944435804269e77363c517f807d5aa01
Aditya Vohra <avohra@yelp.com>
2017-10-26 17:42:53 +0200

    Added post-processing step for documentation

---
+++ src/__tests__/fixtures/component_11.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+ /**
+  * Test for documentation of React components with Flow annotations for props.
+  */
+
+import React from 'react';
+
+type Props = {
+    prop1: string,
+    prop2: string,
+};
+
+class Foo extends React.Component<Props> {
+    render() {
+        return (
+            <div>
+                {this.props.prop1}
+                I am Foo!
+                {this.props.prop2}
+            </div>
+        );
+    }
+}
+
+Foo.defaultProps = {
+    prop2: 'bar',
+};
+
+export default Foo;
---
+++ src/utils/postProcessDocumentation.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ */
+
+function postProcessProps(props) {
+  // props with default values should not be required
+  Object.keys(props).forEach(prop => {
+    const propInfo = props[prop];
+
+    if (propInfo.defaultValue) {
+      propInfo.required = false;
+    }
+  });
+}
+
+export default function (documentation) {
+  const props = documentation.props;
+
+  if (props) {
+    postProcessProps(props);
+  }
+
+  return documentation;
+}
