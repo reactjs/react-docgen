628041ef657adc38cbf48182ad20582619bbf4db
Daniel Tschinder <daniel@tschinder.de>
2019-02-06 22:01:23 -0800

    Create react-docgen playground website (#322)
    * Create react-docgen playground website
    
    * Fix name of browserlist file
    
    * Remove logfile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,7 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: [
-    'eslint:recommended',
-    'prettier'
-  ],
-  plugins:[
-    'prettier'
-  ],
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
   rules: {
     strict: ['error', 'never'],
     'no-shadow': 'error',
@@ -16,11 +11,21 @@ module.exports = {
   },
   env: {
     node: true,
-    es6: true
+    es6: true,
   },
   globals: {
     ASTNode: true,
     NodePath: true,
-    Recast: true
-  }
-}
+    Recast: true,
+  },
+  overrides: [
+    {
+      files: 'website/**/*.js',
+      env: { browser: true },
+      rules: {
+        // conflicts with jsx
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
+};
