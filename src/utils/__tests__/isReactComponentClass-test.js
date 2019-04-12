5582f91f870e9450186ce5b014d8f7e7b0ceda75
Daniel Tschinder <daniel@tschinder.de>
2019-04-12 10:40:26 -0700

    Change license to MIT (#341)

--- src/utils/__tests__/isReactComponentClass-test.js
+++ src/utils/__tests__/isReactComponentClass-test.js
@@ -1,10 +1,8 @@
-/*
- * Copyright (c) 2015, Facebook, Inc.
- * All rights reserved.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  *
  */
 
@@ -57,7 +55,7 @@ describe('isReactComponentClass', () => {
         var React = require('react');
         /**
          * @class Foo
-         * @extends React.Component 
+         * @extends React.Component
          */
         class Foo extends Bar {}
       `).get('body', 1);
