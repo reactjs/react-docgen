aed3bd51c357e3e645c5f6417bfab7f29b281fe5
Daniel Tschinder <daniel@tschinder.de>
2019-04-12 18:47:25 -0700

    Assume HOC first param is Component not last
    Unless we know for sure it is not a component (literal, array, object, spread)

--- src/utils/resolveHOC.js
+++ src/utils/resolveHOC.js
@@ -28,9 +28,23 @@ export default function resolveHOC(path: NodePath): NodePath {
     !isReactForwardRefCall(path)
   ) {
     if (node.arguments.length) {
-      return resolveHOC(
-        resolveToValue(path.get('arguments', node.arguments.length - 1)),
-      );
+      const inner = path.get('arguments', 0);
+
+      // If the first argument is one of these types then the component might be the last argument
+      // If there are all identifiers then we cannot figure out exactly and have to assume it is the first
+      if (
+        node.arguments.length > 1 &&
+        (t.Literal.check(inner.node) ||
+          t.ObjectExpression.check(inner.node) ||
+          t.ArrayExpression.check(inner.node) ||
+          t.SpreadElement.check(inner.node))
+      ) {
+        return resolveHOC(
+          resolveToValue(path.get('arguments', node.arguments.length - 1)),
+        );
+      }
+
+      return resolveHOC(resolveToValue(inner));
     }
   }
 
