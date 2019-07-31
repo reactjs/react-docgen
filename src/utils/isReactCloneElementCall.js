7ba88e20d2708ed2c10712aa3747a81840f89d83
Tongbin Xu <tbxu@freewheel.com>
2019-07-31 10:07:40 +0800

    fix: #380

--- src/utils/isReactCloneElementCall.js
+++ src/utils/isReactCloneElementCall.js
@@ -16,16 +16,24 @@ const { namedTypes: t } = types;
 
 /**
  * Returns true if the expression is a function call of the form
- * `React.createElement(...)`.
+ * `React.cloneElement(...)`.
  */
 export default function isReactCloneElementCall(path: NodePath): boolean {
   if (t.ExpressionStatement.check(path.node)) {
     path = path.get('expression');
   }
 
-  if (!match(path.node, { callee: { property: { name: 'cloneElement' } } })) {
-    return false;
+  if (match(path.node, { callee: { property: { name: 'cloneElement' } } })) {
+    const module = resolveToModule(path.get('callee', 'object'));
+    return Boolean(module && isReactModuleName(module));
   }
-  const module = resolveToModule(path.get('callee', 'object'));
-  return Boolean(module && isReactModuleName(module));
+  /**
+   * `import { cloneElement } from 'react';`
+   * `cloneElement(...)`
+   */
+  if (match(path.node, { callee: { name: 'cloneElement' } } )) {
+    const module = resolveToModule(path.get('callee'));
+    return Boolean(module && isReactModuleName(module));
+  }
+  return false;
 }
