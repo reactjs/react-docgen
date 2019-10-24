80e46286851ed7f20d2b13012db4ca4cdfd28260
Daniel Tschinder <daniel@tschinder.de>
2019-10-24 23:27:47 +0200

    feat: Support destructuring and aliased imports in react builtin call detection

---
+++ src/utils/__tests__/isReactForwardRefCall-test.js
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+import { parse } from '../../../tests/utils';
+import isReactForwardRefCall from '../isReactForwardRefCall';
+
+describe('isReactForwardRefCall', () => {
+  function parsePath(src) {
+    const root = parse(src);
+    return root.get('body', root.node.body.length - 1, 'expression');
+  }
+
+  describe('built in React.forwardRef', () => {
+    it('accepts forwardRef called on React', () => {
+      const def = parsePath(`
+        var React = require("React");
+        React.forwardRef({
+          render() {}
+        });
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+
+    it('accepts forwardRef called on aliased React', () => {
+      const def = parsePath(`
+        var other = require("React");
+        other.forwardRef({
+          render() {}
+        });
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+
+    it('ignores other React calls', () => {
+      const def = parsePath(`
+        var React = require("React");
+        React.isValidElement({});
+      `);
+      expect(isReactForwardRefCall(def)).toBe(false);
+    });
+
+    it('ignores non React calls to forwardRef', () => {
+      const def = parsePath(`
+        var React = require("bob");
+        React.forwardRef({
+          render() {}
+        });
+      `);
+      expect(isReactForwardRefCall(def)).toBe(false);
+    });
+
+    it('accepts forwardRef called on destructed value', () => {
+      const def = parsePath(`
+        var { forwardRef } = require("react");
+        forwardRef({});
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+
+    it('accepts forwardRef called on destructed aliased value', () => {
+      const def = parsePath(`
+        var { forwardRef: foo } = require("react");
+        foo({});
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+
+    it('accepts forwardRef called on imported value', () => {
+      const def = parsePath(`
+        import { forwardRef } from "react";
+        forwardRef({});
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+
+    it('accepts forwardRef called on imported aliased value', () => {
+      const def = parsePath(`
+        import { forwardRef as foo } from "react";
+        foo({});
+      `);
+      expect(isReactForwardRefCall(def)).toBe(true);
+    });
+  });
+});
--- src/utils/isReactForwardRefCall.js
+++ src/utils/isReactForwardRefCall.js
@@ -6,25 +6,12 @@
  *
  * @flow
  */
-import types from 'ast-types';
-import isReactModuleName from './isReactModuleName';
-import match from './match';
-import resolveToModule from './resolveToModule';
-
-const { namedTypes: t } = types;
+import isReactBuiltinCall from './isReactBuiltinCall';
 
 /**
  * Returns true if the expression is a function call of the form
  * `React.forwardRef(...)`.
  */
 export default function isReactForwardRefCall(path: NodePath): boolean {
-  if (t.ExpressionStatement.check(path.node)) {
-    path = path.get('expression');
-  }
-
-  if (!match(path.node, { callee: { property: { name: 'forwardRef' } } })) {
-    return false;
-  }
-  const module = resolveToModule(path.get('callee', 'object'));
-  return Boolean(module && isReactModuleName(module));
+  return isReactBuiltinCall(path, 'forwardRef');
 }
