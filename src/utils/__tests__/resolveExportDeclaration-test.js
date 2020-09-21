6d476efcbcd42980a88cb17a3eb90c4ba73bb71a
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-09-21 13:04:20 -0700

    Switch resolveExportDeclaration tests to use noopImporter

--- src/utils/__tests__/resolveExportDeclaration-test.js
+++ src/utils/__tests__/resolveExportDeclaration-test.js
@@ -8,13 +8,12 @@
 
 jest.mock('../resolveToValue');
 
-import { statement } from '../../../tests/utils';
+import { statement, noopImporter } from '../../../tests/utils';
 import resolveToValue from '../resolveToValue';
 import resolveExportDeclaration from '../resolveExportDeclaration';
 
 describe('resolveExportDeclaration', () => {
   const returnValue = {};
-  const ignoreImports = () => null;
 
   beforeEach(() => {
     resolveToValue.mockReturnValue(returnValue);
@@ -22,60 +21,51 @@ describe('resolveExportDeclaration', () => {
 
   it('resolves default exports', () => {
     const exp = statement('export default 42;');
-    const resolved = resolveExportDeclaration(exp, ignoreImports);
+    const resolved = resolveExportDeclaration(exp, noopImporter);
 
     expect(resolved).toEqual([returnValue]);
-    expect(resolveToValue).toBeCalledWith(
-      exp.get('declaration'),
-      ignoreImports,
-    );
+    expect(resolveToValue).toBeCalledWith(exp.get('declaration'), noopImporter);
   });
 
   it('resolves named exports', () => {
     let exp = statement('export var foo = 42, bar = 21;');
-    let resolved = resolveExportDeclaration(exp, ignoreImports);
+    let resolved = resolveExportDeclaration(exp, noopImporter);
 
     const declarations = exp.get('declaration', 'declarations');
     expect(resolved).toEqual([returnValue, returnValue]);
-    expect(resolveToValue).toBeCalledWith(declarations.get(0), ignoreImports);
-    expect(resolveToValue).toBeCalledWith(declarations.get(1), ignoreImports);
+    expect(resolveToValue).toBeCalledWith(declarations.get(0), noopImporter);
+    expect(resolveToValue).toBeCalledWith(declarations.get(1), noopImporter);
 
     exp = statement('export function foo(){}');
-    resolved = resolveExportDeclaration(exp, ignoreImports);
+    resolved = resolveExportDeclaration(exp, noopImporter);
 
     expect(resolved).toEqual([returnValue]);
-    expect(resolveToValue).toBeCalledWith(
-      exp.get('declaration'),
-      ignoreImports,
-    );
+    expect(resolveToValue).toBeCalledWith(exp.get('declaration'), noopImporter);
 
     exp = statement('export class Foo {}');
-    resolved = resolveExportDeclaration(exp, ignoreImports);
+    resolved = resolveExportDeclaration(exp, noopImporter);
 
     expect(resolved).toEqual([returnValue]);
-    expect(resolveToValue).toBeCalledWith(
-      exp.get('declaration'),
-      ignoreImports,
-    );
+    expect(resolveToValue).toBeCalledWith(exp.get('declaration'), noopImporter);
   });
 
   it('resolves named exports', () => {
     const exp = statement('export {foo, bar, baz}; var foo, bar, baz;');
-    const resolved = resolveExportDeclaration(exp, ignoreImports);
+    const resolved = resolveExportDeclaration(exp, noopImporter);
 
     const specifiers = exp.get('specifiers');
     expect(resolved).toEqual([returnValue, returnValue, returnValue]);
     expect(resolveToValue).toBeCalledWith(
       specifiers.get(0, 'local'),
-      ignoreImports,
+      noopImporter,
     );
     expect(resolveToValue).toBeCalledWith(
       specifiers.get(1, 'local'),
-      ignoreImports,
+      noopImporter,
     );
     expect(resolveToValue).toBeCalledWith(
       specifiers.get(2, 'local'),
-      ignoreImports,
+      noopImporter,
     );
   });
 });
