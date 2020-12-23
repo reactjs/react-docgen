d5e16a02a3c0b067fcbb9511fe500dece5a48f2a
Daniel Tschinder <daniel@tschinder.de>
2020-12-23 15:15:45 +0100

    fix: Correctly support ClassDeclarations without identifier

---
+++ src/__tests__/fixtures/component_41.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface IProps {
+  value: string;
+}
+
+export default class extends React.Component<IProps> {
+  render() {
+    return <div className='s-c-z-example'/>;
+  }
+}
