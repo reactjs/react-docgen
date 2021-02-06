c2f675013f78c377c6dbbdb6bed56834fcec7a9b
Beau Roberts <beau.roberts@autodesk.com>
2021-02-06 20:55:20 +0000

    feat: use @babel/generate to serialise AST nodes w/o loc in `printValue` (#482)
    Resolves #481
    # Conflicts:
    #	yarn.lock

--- src/utils/__tests__/printValue-test.js
+++ src/utils/__tests__/printValue-test.js
@@ -6,6 +6,7 @@
  *
  */
 
+import { builders, NodePath } from 'ast-types';
 import { parse } from '../../../tests/utils';
 import printValue from '../printValue';
 
@@ -20,5 +21,33 @@ describe('printValue', () => {
 
   it('does not print trailing comments', () => {
     expect(printValue(pathFromSource('bar//foo'))).toEqual('bar');
+  });
+
+  it('handles arbitrary generated nodes', () => {
+    expect(
+      printValue(
+        new NodePath(
+          builders.arrayExpression([
+            builders.literal('bar'),
+            builders.literal('foo'),
+            builders.literal(1),
+            builders.literal(2),
+            builders.literal(3),
+            builders.literal(null),
+            builders.memberExpression(
+              builders.identifier('foo'),
+              builders.identifier('bar'),
+            ),
+            builders.jsxElement(
+              builders.jsxOpeningElement(
+                builders.jsxIdentifier('Baz'),
+                [],
+                true,
+              ),
+            ),
+          ]),
+        ),
+      ),
+    ).toEqual('["bar", "foo", 1, 2, 3, null, foo.bar, <Baz />]');
   });
 });
