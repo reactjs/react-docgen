26c90c0dd105bf83499a83826f2a6ff7a724620d
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-03-30 09:35:39 +0000

    chore(website): Use new react 18 createRoot API

--- website/src/index.js
+++ website/src/index.js
@@ -1,9 +1,11 @@
 import 'process';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './components/App';
-
 import 'codemirror/lib/codemirror.css';
 import './react-docgen.less';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
