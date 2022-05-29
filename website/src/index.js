5f691c79f9943a468e234a3bb486cef6b9ffa575
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-05-29 15:53:21 +0200

    chore: Delete website from 5.x branch

--- website/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-import 'codemirror/lib/codemirror.css';
-import './react-docgen.less';
-
-ReactDOM.render(<App />, document.getElementById('root'));
