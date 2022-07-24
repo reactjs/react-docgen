c83a6194eaef998515fcd5b099909969e5bfa92f
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-07-24 12:55:39 +0200

    chore: Clarify comments

--- src/utils/getPropertyValuePath.ts
+++ src/utils/getPropertyValuePath.ts
@@ -4,7 +4,8 @@ import getPropertyName from './getPropertyName';
 
 /**
  * Given an ObjectExpression, this function returns the path of the value of
- * the property with name `propertyName`.
+ * the property with name `propertyName`. if the property is an ObjectMethod we
+ * return the ObjectMethod itself.
  */
 export default function getPropertyValuePath(
   path: NodePath<ObjectExpression>,
