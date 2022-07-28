0e9fbdf0420e3705b46f8d8950334b5552255acd
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-07-29 01:00:39 +0200

    chore: Add test for isReactModuleName

--- src/utils/isReactModuleName.ts
+++ src/utils/isReactModuleName.ts
@@ -9,8 +9,6 @@ const reactModules = [
 /**
  * Takes a module name (string) and returns true if it refers to a root react
  * module name.
- *
- * TODO TESTME
  */
 export default function isReactModuleName(moduleName: string): boolean {
   return reactModules.includes(moduleName.toLowerCase());
