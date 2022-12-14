693c3d469877bf9c6760f8e8d51e4d018def0667
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-12-14 23:22:43 +0100

    Add new website (#701)
    Also fixes the renovate config and disables dep updates in the 5.x branch
    the old website has been renamed to playground until it will be integrated into the new website

---
+++ packages/website/theme.config.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>react-docgen</span>,
+  project: {
+    link: "https://github.com/reactjs/react-docgen",
+  },
+  docsRepositoryBase: "https://github.com/reactjs/react-docgen/tree/main/packages/website",
+  footer: {
+    text: "",
+  },
+};
+
+export default config;
