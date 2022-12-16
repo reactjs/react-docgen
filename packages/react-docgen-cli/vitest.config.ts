4802dfbdab0849a64c3ac0e05da34b27c846a2f2
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-12-16 02:51:05 +0100

    Cleanup and switch to pnpm (#709)

--- packages/react-docgen-cli/vitest.config.ts
+++ packages/react-docgen-cli/vitest.config.ts
@@ -2,17 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: ['./tests/setupTestFramework.ts'],
     include: ['**/__tests__/**/*-test.ts', '**/tests/integration/**/*-test.ts'],
     testTimeout: 30_000,
     deps: {
       interopDefault: false,
     },
     coverage: {
-      all: true,
-      include: ['packages/react-docgen/src/**'],
-      provider: 'c8',
-      reporter: ['text', 'lcov'],
+      include: ['no-coverage'],
     },
   },
 });
