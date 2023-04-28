89be2f3d9cb9e790e3c65c85758950e62269bece
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-28 22:26:33 +0200

    Mark missing content

---
+++ packages/website/src/components/ContentMissing.tsx
@@ -0,0 +1,9 @@
+import { Callout } from 'nextra-theme-docs';
+
+export default function ContentMissing() {
+  return (
+    <Callout emoji="ℹ️" type="info">
+      This content hasn't been created yet.
+    </Callout>
+  );
+}
