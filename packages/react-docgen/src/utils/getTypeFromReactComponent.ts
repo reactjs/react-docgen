a8d4b25765ad8703cec56c0a50f4e17a989d724b
Remo Vetere <remo.vetere@gmail.com>
2024-05-27 13:04:26 +0200

    Add support for generic types on forwardRef

--- packages/react-docgen/src/utils/getTypeFromReactComponent.ts
+++ packages/react-docgen/src/utils/getTypeFromReactComponent.ts
@@ -12,6 +12,7 @@ import type {
   FlowType,
   InterfaceDeclaration,
   InterfaceExtends,
+  Node,
   TSExpressionWithTypeArguments,
   TSInterfaceDeclaration,
   TSType,
@@ -39,6 +40,17 @@ function getStatelessPropsPath(
   if (!value.isFunction()) return;
 
   return value.get('params')[0];
+}
+
+function getForwardRefGenericsType(componentDefinition: NodePath) {
+  const typeParameters = componentDefinition.get(
+    'typeParameters',
+  ) as NodePath<Node>;
+  if (typeParameters && typeParameters.hasNode()) {
+    const params = typeParameters.get('params') as NodePath<Node>[];
+    return params[1] ?? null;
+  }
+  return null;
 }
 
 function findAssignedVariableType(
@@ -106,6 +118,11 @@ export default (componentDefinition: NodePath): NodePath[] => {
       }
     }
   } else {
+    const typeAnnotation = getForwardRefGenericsType(componentDefinition);
+    if (typeAnnotation) {
+      typePaths.push(typeAnnotation);
+    }
+
     const propsParam = getStatelessPropsPath(componentDefinition);
 
     if (propsParam) {
