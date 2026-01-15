d790f1a2f718ddc0320678460a557fa09f4cb33f
Daniel Tschinder <231804+danez@users.noreply.github.com>
2026-01-15 22:26:14 +0100

    Add sitemap to website

---
+++ packages/website/next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+export default {
+  siteUrl: 'https://react-docgen.dev',
+  changefreq: 'weekly',
+  priority: '0.5',
+  generateIndexSitemap: false
+}
