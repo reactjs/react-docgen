eb6759427a9f9e13f08311c587f8cadabacf945a
Daniel Tschinder <code@tschinder.de>
2026-05-05 19:50:42 +0200

    More docs

--- packages/website/src/app/docs/reference/_meta.js
+++ packages/website/src/app/docs/reference/_meta.js
@@ -4,4 +4,5 @@ export default {
   documentation: 'Documentation',
   'file-state': 'FileState',
   handlers: 'Handlers',
+  resolvers: 'Resolvers',
 };
---
+++ packages/website/src/app/docs/reference/resolvers/_meta.js
@@ -0,0 +1,6 @@
+export default {
+  'chain-resolver': 'Chain',
+  'find-all-definitions-resolver': 'FindAllDefinitions',
+  'find-annotated-definitions-resolver': 'FindAnnotatedDefinitions',
+  'find-exported-definitions-resolver': 'FindExportedDefinitions',
+};
