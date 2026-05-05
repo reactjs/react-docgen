9e440adead274a935cf74d9b84556a9541b69487
Daniel Tschinder <code@tschinder.de>
2026-05-05 19:58:35 +0200

    Update docs (#1091)
    * Add security warnings for cli options
    
    * Fix docs
    
    * More docs
    
    * Fix cli

--- packages/website/src/app/docs/reference/_meta.js
+++ packages/website/src/app/docs/reference/_meta.js
@@ -1,7 +1,9 @@
 export default {
+  cli: 'Command Line Interface (CLI)',
   config: 'Config',
   api: 'API',
   documentation: 'Documentation',
   'file-state': 'FileState',
   handlers: 'Handlers',
+  resolvers: 'Resolvers',
 };
